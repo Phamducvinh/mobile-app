5eb0455dd02076bc83382820578439cc982775d7
Duc Viet <viet2k3nguyen@gmail.com>
2024-02-27 17:32:23 +0700

    them categories

--- demo/lib/presentation/screen/categories_screen.dart
+++ demo/lib/presentation/screen/categories_screen.dart
@@ -2,13 +2,105 @@ import "package:demo/presentation/screen/home_screen.dart";
 import "package:flutter/material.dart";
 // import "package:google_nav_bar/google_nav_bar.dart";
 
-class CategoriesScreen extends StatelessWidget{
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello'),
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
+        title: const Text(
+          "Categories",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+        child: GridView.builder(
+          physics: const BouncingScrollPhysics(),
+          itemCount: categoriesName.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              childAspectRatio: 16 / 15,
+              crossAxisCount: 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                // print(categoriesName[index]);
+                // Navigator.push(
+                //     context,
+                //     MaterialPageRoute(
+                //         builder: (context) =>
+                //             BookList(name: categoriesName[index])));
+              },
+              child: Stack(
+                alignment: Alignment.bottomLeft,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      image: DecorationImage(
+                          image: AssetImage(categoriesImage[index]),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        gradient: LinearGradient(
+                          colors: [
+                            Colors.transparent,
+                            Colors.black.withOpacity(0.3)
+                          ],
+                          stops: const [0.6, 0.9],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      categoriesName[index],
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 22
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
 }
+
+List categoriesName = [
+  "Fiction",
+  "Classic",
+  "Romance",
+  "Mystery",
+  "Fantasy",
+  "History",
+  "Comic",
+  "Crime",
+];
+
+List categoriesImage = [
+  "assets/images/categories/anh1.png",
+  "assets/images/categories/anh2.png",
+  "assets/images/categories/anh3.png",
+  "assets/images/categories/anh4.png",
+  "assets/images/categories/anh5.png",
+  "assets/images/categories/anh6.png",
+  "assets/images/categories/anh7.png",
+  "assets/images/categories/anh8.png",
+];
